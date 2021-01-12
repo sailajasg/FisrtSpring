1d6180c513668b467c1442ecf067d40c61aa1f80
sgsailaja <sgsailaja@gmail.com>
2021-01-12 12:25:45 +0530

    first commit

---
+++ MSpringWeb/src/main/java/com/sg/demo/MSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.sg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSpringWebApplication.class, args);
+	}
+
+}
